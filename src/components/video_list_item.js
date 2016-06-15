ffd4bbd91264cdf0b08a065426112084c95af481
Stuart Dotson <stuart.dotson@opower.com>
2016-06-15 17:59:37 -0400

    more progress, near end of section 2

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video, onVideoSelect}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+
 	return (
-		<li>Video</li>
+		<li className="list-group-item" onClick={() => onVideoSelect(video)}>
+			<div className="video-list media">
+				<div className="media-left">
+					<img src={imageUrl} className="media-object" />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">
+						{video.snippet.title}
+					</div>
+				</div>
+			</div>
+		</li>
 	);
 }
 
